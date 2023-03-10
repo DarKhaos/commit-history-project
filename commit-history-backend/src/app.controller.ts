d5dd276a541f4c545e59565b44ac25e70db4448d
Darko Lopez Solis <darkolopezsolis@gmail.com>
2023-03-10 08:23:27 -0400

    Add Logger import and Swagger tags

--- commit-history-backend/src/app.controller.ts
+++ commit-history-backend/src/app.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Get, Query, ServiceUnavailableException } from '@nestjs/common';
+import { Controller, Get, Logger, Query, ServiceUnavailableException } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CommitSummaryDto } from './commit-summary.dto';
 import { GetCommitListDto } from './get-commit-list.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Commits')
 @Controller({ path: 'commits' })
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
   constructor(private readonly appService: AppService) { }
 
+  @ApiResponse({ status: 200, description: 'Get list of project commits.' })
   @Get()
   async getCommitList(@Query() query: GetCommitListDto): Promise<CommitSummaryDto[]> {
     try {
-    return await this.appService.getCommitList(query);
+      return await this.appService.getCommitList(query);
     } catch (e) {
       this.logger.error(e);
       throw new ServiceUnavailableException();
