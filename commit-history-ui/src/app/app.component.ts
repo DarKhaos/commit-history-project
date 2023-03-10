38f182f1e6a0b34561e2df33fae08d222ad2eae9
Darko Lopez Solis <darkolopezsolis@gmail.com>
2023-03-10 08:42:16 -0400

    UI - Refactor App component to use new component

--- commit-history-ui/src/app/app.component.ts
+++ commit-history-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommitListTableComponent } from './commit-list-table/commit-list-table.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'commit-history-ui';
+  @ViewChild(CommitListTableComponent) childComponent!: CommitListTableComponent;
+
+  loading = false;
+
+  latestDate: Date | null = null;
+
+  updateTable(): void {
+    const currentLatestDate = new Date();
+    this.childComponent.getCommitList(this.latestDate);
+    this.latestDate = currentLatestDate;
+  }
+
+  setLoading(loading: boolean): void {
+    this.loading = loading;
+  }
 }
