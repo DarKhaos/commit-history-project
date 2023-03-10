182029ba7c50cc59e79b9db08ab36b6b94796845
Darko Lopez Solis <darkolopezsolis@gmail.com>
2023-03-10 08:25:49 -0400

    Add Swagger decorators to Get Commit List DTO

--- commit-history-backend/src/get-commit-list.dto.ts
+++ commit-history-backend/src/get-commit-list.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDateString, IsOptional } from "class-validator";
 
 export class GetCommitListDto {
+    @ApiProperty({
+        description: 'The date from which commits should be retrieved (ISO 8601 format).',
+        required: false,
+        default: '2023-01-01T00:00:00Z'
+    })
     @IsDateString()
     @IsOptional()
     since?: string;
