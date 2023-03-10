053319e3e5b73c9b8785941a8787a6ea14fed13e
Darko Lopez Solis <darkolopezsolis@gmail.com>
2023-03-10 08:07:06 -0400

    Add ID field to DTO

--- commit-history-backend/src/commit-summary.dto.ts
+++ commit-history-backend/src/commit-summary.dto.ts
@@ -1,13 +1,15 @@
 export class CommitSummaryDto {
+    id: string;
     author: string;
     message: string;
     url: string;
 
     public static from(obj: any): CommitSummaryDto {
-        const { author, commit } = obj;
+        const { sha: id, author, commit } = obj;
         const { login: authorName } = author;
         const { message, url } = commit;
         const dto = new CommitSummaryDto();
+        dto.id = id;
         dto.author = authorName;
         dto.message = message;
         dto.url = url;
